45c074f06f81842d458e863119b9d9bbd5a368b8
Lennard Claproth <lennardclaproth@gmail.com>
2019-03-20 10:23:14 +0100

    Made a few changes to the slider

--- javascript/slider.js
+++ javascript/slider.js
@@ -6,19 +6,24 @@ function plusSlides(n)
   		showSlides(slideIndex += n);
 	}
 
-// function currentSlide(n) 
-// {
-// 	showSlides(slideIndex = n);
-// }
+function currentSlide(n) 
+{
+	showSlides(slideIndex = n);
+}
 
 function showSlides(n)
 {
 	var i;
 	var slides = document.getElementsByClassName("slide_container")
+	var dots = document.getElementsByClassName("dot");
 
 	if(n > slides.length)
 	{
 		slideIndex = 1
+		if(2%2==0)
+		{
+			console.log(3%2);
+		}
 	}
 
 	if(n < 1)
@@ -30,5 +35,11 @@ function showSlides(n)
 	{
 		slides[i].style.display = "none";
 	}
+
+	// for (i = 0; i < dots.length; i++) 
+	// {
+	// 	dots[i].className = dots[i].className.replace("active", "active"); 
+	// }
+
 	slides[slideIndex-1].style.display = "block";
 }
